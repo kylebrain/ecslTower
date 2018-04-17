a058b69f7b684acae151e5aa670b300c4ef18406
Kyle S Brainard <kbrainard@unr.edu>
2018-04-17 15:28:56 -0700

    first commit

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World!");
+	}
+	
+}
