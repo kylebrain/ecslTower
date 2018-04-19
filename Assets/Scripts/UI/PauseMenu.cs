65b9ad6ad1ef97688107a1aa31e7e45d812e1904
Ryan Jantzen <ryan_jantzen@nevada.unr.edu>
2018-04-19 14:10:54 -0700

    Created some of the UI classes

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : Menu
+{
+    protected override void Quit()
+    {
+        // TODO: Implement later
+
+    }
+}
