df9901ed9bfbc9a0fe1be56bf2c3fd0ee65f06cf
Ryan Jantzen <ryan_jantzen@nevada.unr.edu>
2018-04-19 14:55:48 -0700

    Added a free UI pack from the assets store named Modern UI Pack by MICHSKY.

---
+++ Assets/3rdParty/Modern UI Pack/Scripts/ProgressBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ProgressBar : MonoBehaviour {
+
+	[Header("OBJECTS")]
+	public Transform loadingBar;
+	public Transform textPercent;
+
+	[Header("VARIABLES (IN-GAME)")]
+	public bool isOn;
+	public bool restart;
+	[Range(0, 100)] public float currentPercent;
+	[Range(0, 100)] public int speed;
+
+	void Update ()
+	{
+		if (currentPercent < 100 && isOn == true) 
+		{
+			currentPercent += speed * Time.deltaTime;
+		}
+
+		if (currentPercent == 100 || currentPercent >= 100 && restart == true) 
+		{
+			currentPercent = 0;
+		}
+		loadingBar.GetComponent<Image> ().fillAmount = currentPercent / 100;
+		textPercent.GetComponent<Text> ().text = ((int)currentPercent).ToString ("F0") + "%";
+	}
+}
