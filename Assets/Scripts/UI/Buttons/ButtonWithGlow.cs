9ad366ef127974bed15dd55543a7c3fa1f358b61
Ryan Jantzen <ryan_jantzen@nevada.unr.edu>
2018-04-19 16:26:40 -0700

    Added the buttons and started working on the button glow.

---
+++ Assets/Scripts/UI/Buttons/ButtonWithGlow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonWithGlow : MonoBehaviour
+{
+    public ButtonGlow buttonGlow;
+
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    private void Start()
+    {
+        if (buttonGlow == null)
+        {
+            Debug.Log("There is no assigned button glow component");
+        }
+        // TODO: Set button events
+    }
+}
