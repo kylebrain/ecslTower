0fbe4228dc7fd3934ef7844ccf3af38624cc5719
Kyle Brainard <kbrainard@nevada.unr.edu>
2018-04-19 16:18:30 -0700

    WavePaths can now be created through the WaveManager and Agent will follow path. Test only works for one Wave/Path/Agent.

--- Assets/Scripts/Wave/Wave.cs
+++ Assets/Scripts/Wave/Wave.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class Wave : MonoBehaviour {
 
+    public List<WavePath> pathList = new List<WavePath>();
+
+    //** test area **//
+    public Agent agent;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if(pathList.Count > 0)
+            {
+                Spawn(agent, pathList[0]);
+            }
+        }
+    }
+
+    //** End of test area **//
+
     public void Spawn(Agent agentPrefab, WavePath path)
     {
-        Agent newAgent = Instantiate(agentPrefab) as Agent;
+        Agent newAgent = Instantiate(agentPrefab, path.GetNextNode().transform) as Agent;
         newAgent.BeginMovement(path);
     }
 }
--- Assets/Scripts/Wave/WaveManager.cs
+++ Assets/Scripts/Wave/WaveManager.cs
@@ -2,16 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveManager : MonoBehaviour {
+public class WaveManager : MonoBehaviour
+{
 
-    public Grid grid;
+    public WorldGrid worldGrid;
     List<Wave> waveList = new List<Wave>();
-    private Node currStart;
-    private Node currEnd;
+    private Wave currentWave;
+    private Node currStart = null;
+    private Node currEnd = null;
+    public Wave wavePrefab;
 
-    private void OnMouseDown()
+    //test
+
+    private void Start()
     {
-        
+        Wave newWave = Instantiate(wavePrefab, this.transform) as Wave;
+        waveList.Add(newWave);
+        currentWave = waveList[0];
     }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (currStart != null)
+            {
+                currEnd = worldGrid.getRaycastNode();
+                if(currEnd == currStart)
+                {
+                    currEnd = null;
+                }
+                if (currEnd != null)
+                {
+                    Debug.Log("End Point set to: " + currEnd.name);
+                }
+            }
+            else
+            {
+                currStart = worldGrid.getRaycastNode();
+                if (currStart != null)
+                {
+                    Debug.Log("Start Point set to: " + currStart.name);
+                }
+            }
+
+            if (currStart != null && currEnd != null)
+            {
+                WavePath newPath = new WavePath();
+                newPath.InitializePath(currStart, currEnd);
+                currentWave.pathList.Add(newPath);
+            }
+        }
+    }
+
 
 }
