deb6a5fd80a9271526d7d7bed3daf9500e3326b0
Eryl Kenner <eryl.kenner@nevada.unr.edu>
2018-04-19 16:57:19 -0700

    Commiting to fix git issue

--- Assets/Scripts/Grid/WorldGrid.cs
+++ Assets/Scripts/Grid/WorldGrid.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldGrid : MonoBehaviour {
+public class WorldGrid: MonoBehaviour {
     public int rows = 1;
     public int cols = 1;
     public Node nodePrefab;
 
     private Node[,] m_grid;
-    
+
 
     private void Awake() {
         m_grid = new Node[rows, cols];
@@ -22,19 +22,31 @@ public class WorldGrid : MonoBehaviour {
         }
     }
 
-    public Node getAt(int x, int y) {
-        if(x > rows || y > cols) {
-            return null;
-        }
-        return m_grid[x, y];
-    }
-
-    private void LateUpdate() {
+    /// <summary>
+    /// Raycasts the mouse location to the WorldGrid.
+    /// Returns the node it hit, or null if no collision occured.
+    /// </summary>
+    /// <returns>The hit node if valid, otherwise null</returns>
+    public Node getRaycastNode() {
         Ray ray;
         RaycastHit hit;
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hit)) {
-            hit.transform.gameObject.GetComponent<Node>().setHovered();
+        if(!Physics.Raycast(ray, out hit)) {
+            return null;
+        }
+
+        Node ret = hit.transform.gameObject.GetComponent<Node>();
+        if(ret != null) {
+            return ret;
+        } else {
+            return null;
+        }
+    }
+
+    private void LateUpdate() {
+        Node hit = getRaycastNode();
+        if(hit != null) {
+            hit.setHovered();
         }
     }
 
