164f3daaf28cdb3a02d1367c3b98ac99b728903b
Kyle Brainard <kbrainard@nevada.unr.edu>
2018-04-19 13:54:08 -0700

    Created Wave folder and class file

---
+++ Assets/Scripts/Wave/Wave.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("This is a wave!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
