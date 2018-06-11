bd7d17a060657a09bb818f5d9edc7f9f1349aed7
Kyle-PC <kbrainard@nevada.unr.edu>
2018-06-11 14:31:08 -0700

    Sell button allows return of full price when placing the Router.

--- Assets/Scripts/UI/SubMenus/Game/RouterOptions/Sell.cs
+++ Assets/Scripts/UI/SubMenus/Game/RouterOptions/Sell.cs
@@ -5,18 +5,26 @@ using UnityEngine.UI;
 
 public class Sell: GameButton {
     private RouterBuilding root;
+    private int currentPrice; 
 
     private void Start()
     {
         root = transform.root.GetComponent<RouterBuilding>();
-        transform.Find("Button").Find("Text").GetComponent<Text>().text = "Sell: $" + root.sellPrice;
+        currentPrice = root.price;
+        transform.Find("Button").Find("Text").GetComponent<Text>().text = "Return: $" + currentPrice;
+        StartCoroutine(waitForPlace());
     }
 
     public override void PerformAction() {
-
-        int returnMoney = root.sellPrice;
-        Score.score += returnMoney;
-
+        Score.score += currentPrice;
         root.removeFromMap();
+        Building.currentlyPlacing = false; //if you are able to select a building when placing one this is wrong, currently works
+    }
+
+    IEnumerator waitForPlace()
+    {
+        yield return new WaitUntil(() => root.Placed);
+        currentPrice = root.sellPrice;
+        transform.Find("Button").Find("Text").GetComponent<Text>().text = "Sell: $" + currentPrice;
     }
 }
